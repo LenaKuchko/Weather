af992fd1e226d681e1eebdcee578137d32cc2c65
Arianna Chau and Olena Kuchko <arianna+olena@>
2017-06-27 17:00:36 -0700

    wright way

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,6 @@
 var apiKey = require('./../.env').apiKey;
 var Weather = function () {
+
 }
 Weather.prototype.toCelsius = function(kelvin) {
   return kelvin - 273.15;
@@ -8,7 +9,7 @@ Weather.prototype.toCelsius = function(kelvin) {
 Weather.prototype.getWeather = function (city, displayWeather) {
   $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey)
   .then(function(response) {
-  displayWeather(city, response.main.humidity, response.main.temp, (response.main.temp - 273.15));
+  displayWeather(city, response.main.humidity, response.main.temp, (this.toCelcius(response.main.temp)));
   }).fail(function(error) {
     $('.showWeather').text(error.responseJSON.message);
   });
